532129ce02ff7a48127372aabe76c297c51f110b
Travis Nelson <tjnel6691@gmail.com>
2022-11-21 15:45:14 -0500

    Added delete student button

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -3,48 +3,66 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-
 const Students = () => {
-    const [students, setStudents] = useState([])
-  
-    useEffect(() => {
-        const getStudentsDetails = async () => {
-            const response = await axios.get(
-                `https://damp-peak-71043.herokuapp.com/school/grade/retrieve`
-            )
-            setStudents(response.data)
-            console.log(response.data)
-        }
-        getStudentsDetails()
-    }, [])
+  const [students, setStudents] = useState([])
 
-    return (
-        <div className='bg-dark'>
-            <Link to="/students/new_student">
-                <button>Add New Student</button>
-            </Link>
-            <section className='container'>
-                <div className='row text-light'>
-                    {students?.map((student) => (
-                        <div className='col-3 border border-success rounded wrap m-4 p-4 student-card'>
-                            <div>
-                                <h3>{student.name}</h3>
-                                <h5 className='d-flex flex-wrap email-text'> <p className='text-warning'>E-mail:</p> {student.email}</h5>
-                            </div>
-                            <div className=''>
-                                {student.student_course?.map((course) => (
-                                    <div className='d-flex flex-wrap'>                                                   
-                                        <h5>{course.name}&nbsp;&nbsp;</h5> 
-                                        <h5 className='text-info'>"{course.Grade.score}"</h5>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    ))}    
-                </div>
-            </section>
-        </div>
+  const handleDelete = async (id) => {
+    await axios
+      .delete(
+        `https://damp-peak-71043.herokuapp.com/school/student/delete/${id}`
+      )
+      .then(() => {
+        console.log(`Deleted student with id of ${id}`)
+        getStudentsDetails()
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  const getStudentsDetails = async () => {
+    const response = await axios.get(
+      `https://damp-peak-71043.herokuapp.com/school/grade/retrieve`
     )
+    setStudents(response.data)
+    console.log(response.data)
+  }
+
+  useEffect(() => {
+    getStudentsDetails()
+  }, [])
+
+  return (
+    <div className="bg-dark">
+      <Link to="/students/new_student">
+        <button>Add New Student</button>
+      </Link>
+      <section className="container">
+        <div className="row text-light">
+          {students?.map((student) => (
+            <div className="col-3 border border-success rounded wrap m-4 p-4 student-card">
+              <div>
+                <h3>{student.name}</h3>
+                <button onClick={() => handleDelete(student.id)}>Delete</button>
+                <h5 className="d-flex flex-wrap email-text">
+                  {' '}
+                  <p className="text-warning">E-mail:</p> {student.email}
+                </h5>
+              </div>
+              <div className="">
+                {student.student_course?.map((course) => (
+                  <div className="d-flex flex-wrap">
+                    <h5>{course.name}&nbsp;&nbsp;</h5>
+                    <h5 className="text-info">"{course.Grade.score}"</h5>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default Students
