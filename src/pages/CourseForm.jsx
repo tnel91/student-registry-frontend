6ff1f3f5b21f2ee9b00e8c4f50a75a7f9314765f
Brian Lara <Briandlara1@gmail.com>
2022-11-22 11:07:13 -0500

    added css to forms

--- src/pages/CourseForm.jsx
+++ src/pages/CourseForm.jsx
@@ -27,12 +27,12 @@ const CourseForm = () => {
     }
 
     return (
-        <div>
+        <div className='d-flex justify-content-center align-items-center align-self-center m-5 p-5'>
             <form onSubmit={handleSubmit}>
-                <div className="form-group">
+                <div className="form-group p-3 px-5">
                     <label htmlFor="course_number">Course Number</label>
                     <input 
-                    className="form-control" 
+                    className="form-control pl-5" 
                     id="course_number" 
                     type="text" 
                     placeholder="Write The Course Number Here" 
@@ -41,10 +41,10 @@ const CourseForm = () => {
                     required
                     />
                 </div>
-                <div className="form-group">
+                <div className="form-group p-3 px-5">
                     <label htmlFor="name">Course Name</label>
                     <input 
-                    className="form-control" 
+                    className="form-control pl-5" 
                     id="name" 
                     type="text" 
                     placeholder="Write The Course Name Here" 
@@ -53,10 +53,10 @@ const CourseForm = () => {
                     required
                     />
                 </div>
-                <div className="form-group">
+                <div className="form-group p-3 px-5">
                     <label htmlFor="credit_hours">Credit Hour</label>
                     <input 
-                    className="form-control" 
+                    className="form-control pl-5" 
                     id="credit_hours" 
                     type="number" 
                     placeholder="Write How Many Credit Hours The Course Is Worth" 
@@ -65,7 +65,7 @@ const CourseForm = () => {
                     required
                     />
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary ms-5">Submit</button>
             </form>
 
 
