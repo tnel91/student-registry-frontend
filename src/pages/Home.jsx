8d3de7ba15329c2b5e7ccfeaaaca948193684f0d
Travis Nelson <tjnel6691@gmail.com>
2022-11-22 09:03:46 -0500

    Styled home page some

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,42 +1,72 @@
 import React from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  let navigate = useNavigate()
   return (
-    <div className="p-5 mt-2">
-      <div className="container">
+    <div>
+      <div class="row">
         <div
-          class="align-self-center col-md-6 text-dark d-none d-md-block "
+          class="align-self-center col-md-6 text-dark d-none d-md-block"
           id="hp-syllabus-background-img"
         >
           <div class="container">
             <Link to="/syllabus">
-              <button class="btn btn-primary btn-lg my-5 ms-5 syllabus-button">
+              <button class="btn btn-primary btn-lg my-5 ms-5">Syllabus</button>
+            </Link>
+          </div>
+        </div>
+        <div class="col-md-6  bg-dark text-light">
+          <div class="container py-5">
+            <h4 className="text-warning pt-5">Syllabus</h4>
+            <p>View all available classes.</p>
+            <Link to="/syllabus">
+              <button class="btn btn-primary btn-lg my-5 ms-5  d-md-none">
                 Syllabus
               </button>
             </Link>
           </div>
         </div>
+      </div>
+      <div class="row">
         <div
           class="align-self-center col-md-6 text-dark d-none d-md-block "
           id="hp-courses-background-img"
         >
           <div class="container">
             <Link to="/courses">
-              <button class="btn btn-primary btn-lg my-5 ms-5 syllabus-button">
+              <button class="btn btn-primary btn-lg my-5 ms-5">Courses</button>
+            </Link>
+          </div>
+        </div>
+        <div class="col-md-6  bg-dark text-light">
+          <div class="container py-5">
+            <h4 className="text-warning pt-5">Courses</h4>
+            <p>View all courses.</p>
+            <Link to="/courses">
+              <button class="btn btn-primary btn-lg my-5 ms-5  d-md-none">
                 Courses
               </button>
             </Link>
           </div>
         </div>
+      </div>
+      <div class="row">
         <div
           class="align-self-center col-md-6 text-dark d-none d-md-block "
           id="hp-students-background-img"
         >
           <div class="container">
             <Link to="/students">
-              <button class="btn btn-primary btn-lg my-5 ms-5 syllabus-button">
+              <button class="btn btn-primary btn-lg my-5 ms-5">Students</button>
+            </Link>
+          </div>
+        </div>
+        <div class="col-md-6  bg-dark text-light">
+          <div class="container py-5">
+            <h4 className="text-warning pt-5">Students</h4>
+            <p>View all students.</p>
+            <Link to="/students">
+              <button class="btn btn-primary btn-lg my-5 ms-5  d-md-none">
                 Students
               </button>
             </Link>
@@ -44,6 +74,52 @@ const Home = () => {
         </div>
       </div>
     </div>
+    // <div className="p-4 mt-0">
+    //   <div className="container">
+    //     <div className="row">
+    //       <div
+    //         class="align-self-center col-md-6 text-dark d-md-block "
+    //         id="hp-syllabus-background-img"
+    //       >
+    //         <div class="container">
+    //           <Link to="/syllabus">
+    //             <button class="btn btn-primary btn-lg my-5 ms-5">
+    //               Syllabus
+    //             </button>
+    //           </Link>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className="row">
+    //       <div
+    //         class="align-self-center col-md-6 text-dark d-md-block "
+    //         id="hp-courses-background-img"
+    //       >
+    //         <div class="container">
+    //           <Link to="/courses">
+    //             <button class="btn btn-primary btn-lg my-5 ms-5">
+    //               Courses
+    //             </button>
+    //           </Link>
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className="row">
+    //       <div
+    //         class="align-self-center col-md-6 text-dark d-md-block "
+    //         id="hp-students-background-img"
+    //       >
+    //         <div class="container">
+    //           <Link to="/students">
+    //             <button class="btn btn-primary btn-lg my-5 ms-5">
+    //               Students
+    //             </button>
+    //           </Link>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
